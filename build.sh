f38b0966e092d227d3b166341556a3643fc46039
Mads Hjorth <madsh@me.com>
2019-01-07 14:45:14 +0100

    frontpage update

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
+curl https://api.csswg.org/bikeshed/ -F file=@start.md -F force=1 > docs/start.html
 curl https://api.csswg.org/bikeshed/ -F file=@intro.md -F force=1 > docs/intro.html
-curl https://api.csswg.org/bikeshed/ -F file=@intro.md -F force=1 > docs/start.html
 #bikeshed spec -f --print=plain spec.md spec.html
 curl https://api.csswg.org/bikeshed/ -F file=@spec.md -F force=1 > docs/spec.html
 #curl https://api.csswg.org/bikeshed/ -F file=@guide.md -F force=1 > guide.html
--- docs/index.md
+++ docs/index.md
@@ -2,5 +2,7 @@ this is new
 
 
 Den seneste version af
-- [start](start.html)
-- [specifikationen](spec.html)
+- [start](start.html) (besluttet 2018-10-29)
+- [intro](intro.html) (version 1 planlagt til 2019-03-01)
+- [specifikation](spec.html) (version 1 planlagt til 2019-03-01)
+- [guide](guide.html) (version 1 planlagt til 2019-03-01)
