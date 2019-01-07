99d2953295ee4ba846ec1631848d83f4f0d025dd
Mads Hjorth <madsh@me.com>
2019-01-07 15:00:31 +0100

    fix svg

--- build.sh
+++ build.sh
@@ -3,3 +3,6 @@ curl https://api.csswg.org/bikeshed/ -F file=@intro.md -F force=1 > docs/intro.h
 #bikeshed spec -f --print=plain spec.md spec.html
 curl https://api.csswg.org/bikeshed/ -F file=@spec.md -F force=1 > docs/spec.html
 #curl https://api.csswg.org/bikeshed/ -F file=@guide.md -F force=1 > guide.html
+
+
+cp high_level.svg cloud.svg docs/
---
+++ index.md
@@ -0,0 +1,5 @@
+Den seneste version af
+- [start](start.html) (besluttet 2018-10-29)
+- [intro](intro.html) (version 1 planlagt til 2019-03-01)
+- [specifikation](spec.html) (version 1 planlagt til 2019-03-01)
+- [guide](guide.html) (version 1 planlagt til 2019-03-01)
