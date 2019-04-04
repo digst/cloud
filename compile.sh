f3aae8e9196073ffe8055713a6c2acf7b31d2d7a
Mads Hjorth <madsh@me.com>
2019-04-04 11:05:04 +0200

    build note

--- compile.sh
+++ compile.sh
@@ -1,4 +1,5 @@
-curl https://api.csswg.org/bikeshed/ -F file=@start.md -F force=1 > docs/start.html
+#curl https://api.csswg.org/bikeshed/ -F file=@start.md -F force=1 > docs/start.html
+curl https://api.csswg.org/bikeshed/ -F file=@clarify.md -F force=1 > docs/clarify.html
 curl https://api.csswg.org/bikeshed/ -F file=@intro.md -F force=1 > docs/intro.html
 #bikeshed spec -f --print=plain spec.md spec.html
 curl https://api.csswg.org/bikeshed/ -F file=@spec.md -F force=1 > docs/spec.html
